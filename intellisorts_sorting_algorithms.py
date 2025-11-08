596e18491db3f627a5154cdb5bfcb2f5f99c5335
manuelohrndorf <manuel.ohrndorf@gmx.net>
2025-11-08 22:02:24 +0100

    revert

--- intellisorts_sorting_algorithms.py
+++ intellisorts_sorting_algorithms.py
@@ -16,13 +16,11 @@ def insertion_sort(A):
     for i in range(1, len(A)):
         key = A[i]
         j = i - 1
-        while j >= 0:
+        comparisons += 1
+        while j >= 0 and A[j] > key:
             comparisons += 1
-            if A[j] > key:
-                A[j + 1] = A[j]
-                j -= 1
-            else:
-                break
+            A[j + 1] = A[j]
+            j -= 1
         A[j + 1] = key
     return comparisons
 
@@ -60,8 +58,6 @@ def merge_sort(A):
     k = 0
     
     while i < len(left) and j < len(right):
-        comparecount += 1
-        comparecount += 1
         comparecount += 1
         if left[i] <= right[j]:
             A[k] = left[i]
